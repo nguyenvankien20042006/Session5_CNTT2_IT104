50ec00830ca75a3619f370442fb731efe016490c
Nguyễn Văn Kiên <vankien.200406@gmail.com>
2025-09-09 21:16:20 +0700

    submit

--- PTIT_IT104_CNTT2_Session22/PTIT_IT104_CNTT2_Session22/src/App.tsx
+++ PTIT_IT104_CNTT2_Session22/PTIT_IT104_CNTT2_Session22/src/App.tsx
@@ -1,9 +1,20 @@
+import AlertTypes from "./components/AlertTypes";
+import Cards from "./components/Cards";
+import DropDown from "./components/DropDown";
+import InputTypes from "./components/InputTypes";
+import SpinnerTypes from "./components/SpinnerTypes";
+import TypesExample from "./components/TypesExample";
 
 
 export default function App() {
   return (
     <div>
-      
+      <AlertTypes/>
+      <Cards/>
+      <DropDown/>
+      <InputTypes/>
+      <SpinnerTypes/>
+      <TypesExample/>
     </div>
   )
 }
